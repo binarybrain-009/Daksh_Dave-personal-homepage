e9d209ec13d5c308076403a6869145df54c799b8
Daksh Dave <dakshdave51093@gmail.com>
2021-09-18 22:21:01 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with  ❤️ by <a href="http://www.adityaketkar.me"> Daksh Dave</a>  
+         Built with  ❤️ by <a href="https://www.linkedin.com/in/daksh-dave-61a0ba178/"> Daksh Dave</a>  
         <br/>
         </p>
     </footer>
